5926573f639e61b1e9ad9e9507141af7dbe1de3c
pawaret <62021393@up.ac.th>
2022-03-25 00:06:06 +0700

    Add Navbar

--- components/navbar.js
+++ components/navbar.js
@@ -4,7 +4,9 @@ const Navbar = () => {
   return (
     <div className="h-12 w-full flex justify-between shadow-md">
       <div className="w-3/12">Addd</div>
-      <div className="w-6/12">Addd</div>
+      <div className="w-6/12">
+        <div className="h-10/12"></div>
+      </div>
       <div className="w-3/12">Addd</div>
     </div>
   );
--- pages/cafe/index.js
+++ pages/cafe/index.js
@@ -1,31 +1,57 @@
-import React, {useState} from 'react';
-import styles from "../../styles/Home.module.css";
+import React, { useState } from "react";
+import { create as ipfsHttpClient } from "ipfs-http-client";
+
+const client = ipfsHttpClient("https://ipfs.infura.io:5001/api/v0");
+
 const Login = () => {
-  const [formInput, setFormInput] = useState({
-    email: "",
-    password: "",
-  });
+  const [fileUrl, setFileUrl] = useState("");
+  const [image, setImage] = useState({ preview: "", raw: "" });
+  const [formInput, setFormInput] = useState({});
+
+  const handleImageChange = async (e) => {
+    const file = e.target.files[0];
+    try {
+      const added = await client.add(file);
+      const url = `https://ipfs.infura.io/ipfs/${added.path}`;
+      setFileUrl(url);
+      setImage({
+        preview: url,
+        raw: e.target.files[0],
+      });
+    } catch (error) {
+      console.log("Error uploading file: ", error);
+    }
+  };
 
   const handleSubmit = () => {
     console.log(formInput);
-    axios.post('link', formInput).then(
-      //doSomething
-    )
   };
 
   return (
-    <div className={styles.container}>
+    <div className="">
       <p>Create Next App</p>
-      <p>Maprang</p>
-      <form onSubmit={handleSubmit}>
+      <form onSubmit={handleSubmit} action="#">
+        <p>{fileUrl}</p>
+        {image.preview ? (
+          <img
+            src={image.preview}
+            alt="dummy"
+            className="h-32 w-32 object-cover rounded-xl"
+          />
+        ) : (
+          <div className="h-32 w-32 z-10 flex justify-center items-center rounded-xl"></div>
+        )}
+        <input type="file" onChange={handleImageChange} />
         <input
           type="text"
+          className="border-2 border-black"
           onChange={(e) => {
             setFormInput({ ...formInput, email: e.target.value });
           }}
         />
         <input
           type="text"
+          className="border-2 border-black"
           onChange={(e) => {
             setFormInput({ ...formInput, password: e.target.value });
           }}
